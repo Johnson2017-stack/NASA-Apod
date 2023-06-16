7637c714ffcc9e8305480111a6ebe58907022fd4
Andrew Johnson <andrew.k.johnson228@gmail.com>
2023-06-15 18:02:11 -0700

    added ejs, body-parser, and express, started setting up server

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const ejs = require('ejs');
-const bodyParser = require('body_parser')
+const bodyParser = require('body-parser')
 
 const server = express();
 
+const url = 'https://api.nasa.gov/planetary/apod?api_key=VLXRS1OhKA4gEpO1t9tPPU5cWWLU7ps8ousjyZiG'
+
+server.set('views', './src/views');
+server.set('view engine', 'ejs')
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+app.use(bodyParser.json())
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
