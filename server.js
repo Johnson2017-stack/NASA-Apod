8f506690eb05c7169d697fd3e8a7e5e30d29774c
Andrew Johnson <andrew.k.johnson228@gmail.com>
2023-06-17 12:19:07 -0700

    add prev-pic page, new styles and ejs-lint

--- server.js
+++ server.js
@@ -6,16 +6,27 @@ const server = express();
 server.use('/public', express.static('public'));
 
 const url = 'https://api.nasa.gov/planetary/apod?api_key=VLXRS1OhKA4gEpO1t9tPPU5cWWLU7ps8ousjyZiG'
+const prevPicUrl = 'https://api.nasa.gov/planetary/apod?api_key=VLXRS1OhKA4gEpO1t9tPPU5cWWLU7ps8ousjyZiG&start_date=2023-06-09&end_date=2023-06-17'
 
 server.set('views', './src/views');
 server.set('view engine', 'ejs')
+async:true;
 
-server.get('/nasa', (req, res) => {
+server.get('/home', (req, res) => {
     fetch(url)
     .then((response) => response.json())
     .then((data) => {
         console.log(data)
-        res.render("nasa", {nasaData: data})
+        res.render("home", {nasaData: data})
+    })
+})
+
+server.get('/prev-pic', (req, res) => {
+    fetch(prevPicUrl)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+        res.render("prev-pic", {nasaData: data})
     })
 })
 
