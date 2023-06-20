6de585e6f60aea8412ec6d579fcfd17e3cc455cd
Andrew Johnson <andrew.k.johnson228@gmail.com>
2023-06-19 17:47:46 -0700

    added pagination to server

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const server = express();
 server.use('/public', express.static('public'));
 
 const url = 'https://api.nasa.gov/planetary/apod?api_key=VLXRS1OhKA4gEpO1t9tPPU5cWWLU7ps8ousjyZiG'
-const prevPicUrl = 'https://api.nasa.gov/planetary/apod?api_key=VLXRS1OhKA4gEpO1t9tPPU5cWWLU7ps8ousjyZiG&start_date=2023-06-09&end_date=2023-06-17'
+const prevPicUrl = 'https://api.nasa.gov/planetary/apod?api_key=VLXRS1OhKA4gEpO1t9tPPU5cWWLU7ps8ousjyZiG&start_date=2023-05-25&end_date=2023-06-17'
 
 server.set('views', './src/views');
 server.set('view engine', 'ejs')
-async:true;
+
 
 server.get('/home', (req, res) => {
     fetch(url)
@@ -26,7 +26,16 @@ server.get('/prev-pic', (req, res) => {
     .then((response) => response.json())
     .then((data) => {
         console.log(data)
-        res.render("prev-pic", {nasaData: data})
+
+        const five = 5;
+        let prevPicArr = [];
+        for ( let i = 0; i < data.length; i+= five) {
+            prevPicArr.push(data.slice(i, i+five))
+        }
+
+        
+
+        res.render("prev-pic", {nasaData: prevPicArr})
     })
 })
 
