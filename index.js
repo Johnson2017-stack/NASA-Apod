d66ad5011f160b5c70a61d943150904b937fd313
Andrew Johnson <andrew.k.johnson228@gmail.com>
2023-06-20 03:31:36 -0700

    added pagination

--- index.js
+++ index.js
@@ -21,36 +21,49 @@ server.get("/", (req, res) => {
     });
 });
 
-server.get("/prev-pic", (req, res) => {
-  fetch(prevPicUrl)
+server.get("/prev-pic/:id", (req, res) => {
+    const index = req.params.id;
+    
+    fetch(prevPicUrl)
     .then((response) => response.json())
     .then((data) => {
       // console.log(data);
 
       //   function for infinite scroll
 
-      (function (data) {
-        "use strict";
-        function sleep(ms) {
-          return new Promise((resolve) => setTimeout(resolve, ms));
-        }
+      // (function (data) {
+      //   "use strict";
+      //   function sleep(ms) {
+      //     return new Promise((resolve) => setTimeout(resolve, ms));
+      //   }
 
-        async function main() {
-          scrollTo(0, 30000000);
-          var Maxscroll = scrollY;
-          scrollTo(0, 0);
-          var i = 0;
-          while (i <= Maxscroll) {
-            scrollBy(0, i);
-            await sleep(1);
-            scrollTo(0, 0);
-            i = i + 5;
-          }
-          main();
-        }
-        main();
-      });
-      res.render("prev-pic", { nasaData: data });
+      //   async function main() {
+      //     scrollTo(0, 30000000);
+      //     var Maxscroll = scrollY;
+      //     scrollTo(0, 0);
+      //     var i = 0;
+      //     while (i <= Maxscroll) {
+      //       scrollBy(0, i);
+      //       await sleep(1);
+      //       scrollTo(0, 0);
+      //       i = i + 5;
+      //     }
+      //     main();
+      //   }
+      //   main();
+      // });
+      // res.render("prev-pic", { nasaData: data });
+
+      const five = 5;
+      let prevPicArr = [];
+      for ( let i = 0; i < data.length; i+= five) {
+          prevPicArr.push(data.slice(i, i+five))
+      }
+
+
+
+      res.render("prev-pic", {nasaData: prevPicArr[index]})
+      
     });
 });
 
